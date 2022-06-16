07c14817725af865b61e2c5a17b7306cb861ef3e
Yuzuki Arai <yudukikun5120@gmail.com>
2022-06-16 22:26:52 +0900

    feature: Enhanced application descriptions

--- pages/index.vue
+++ pages/index.vue
@@ -8,25 +8,27 @@
       </h1>
       <div class="flex flex-col gap-y-6">
         <p>
-          漢字凝集解析では，<a
-            class="bg-blue-100"
-            href="https://ja.wikipedia.org/wiki/K平均法"
-            >k-平均法</a
-          >にゟクラスタリングされた漢字群に基づき，あなたが入力した漢字に類似する漢字を表示することができます。
+          漢字凝集解析では，入力した漢字に類似する漢字を検知することができます。
         </p>
 
         <h3 class="font-serif text-3xl font-extrabold -mb-2">概要</h3>
         <p>
-          k-平均法でJIS第一水準の漢字のクラスタリングを行い，適合推定器を作成しました。
+          <a
+            class="bg-blue-100"
+            href="https://ja.wikipedia.org/wiki/K平均法"
+            ><span v-katex="'\k'"></span>-平均法</a
+          >（<span v-katex="'\k=100'"></span> に設定）を用い，JIS第一水準に分類された2965字種をクラスタリングし，類似する漢字を検知します。
+          <br>
+          漢字凝集解析はAPIを提供しています。<a href="https://kanji-clustering.herokuapp.com/docs" class="bg-blue-100">ドキュメント</a>をご覧ください。
         </p>
 
         <h3 class="font-serif text-3xl font-extrabold -mb-2">広告</h3>
         <p>
-          漢字検定を勉強されている方向けの<NuxtLink
+          漢字検定を勉強されている方向けに，<a
             class="bg-blue-100"
-            to="/advertisement"
-            >他のサービス</NuxtLink
-          >も提供しています。
+            href="https://yudukikun5120.notion.site/1f06cf758050421eadf9bb07a250f63b"
+            >クイズアプリや電子書籍</a
+          >を提供しています。
         </p>
 
         <address>
@@ -48,10 +50,18 @@
   </div>
 </template>
 
-<script lang="ts">
+<script lang="js">
 import Vue from "vue";
+import VueKatex from 'vue-katex';
+import 'katex/dist/katex.min.css';
 import KanjiCard from "~/components/KanjiCard.vue";
 import SearchResult from "~/components/SearchResult.vue";
+
+Vue.use(VueKatex, {
+  globalOptions: {
+    //... Define globally applied KaTeX options here
+  }
+});
 
 export default Vue.extend({
   name: "IndexPage",
