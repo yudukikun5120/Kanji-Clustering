109c2fbfdc8b7b7d287992f6f2c1744b1124cdf8
Yuzuki Arai <yudukikun5120@gmail.com>
2022-06-21 22:46:23 +0900

    chore: Set the `lang` as `ja`

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,6 +53,12 @@ export default {
     "@nuxtjs/pwa",
   ],
 
+  pwa: {
+    manifest: {
+      lang: 'ja',
+    },
+  },
+
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: ["@nuxt/http", "@nuxtjs/toast", "nuxt-clipboard"],
 
