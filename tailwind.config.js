17b8746240a9cbf5897e70fa60ef07bebea34985
Yuzuki Arai <arai@yudukikun5120.me>
2025-06-17 00:33:39 +0900

    chore!: update yarn deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  purge: [],
-  darkMode: 'media', // or 'media' or 'class'
+  purge: [
+    "./components/**/*.{vue,js,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}"
+  ],
+  darkMode: 'media',
   theme: {
     extend: {},
   },
